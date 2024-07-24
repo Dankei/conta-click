59bddd13e83b1d3c2962e19099fe5e82f94c355a
Daniel_PC <galvaomartinsdaniel@gmail.com>
2024-07-24 15:45:46 -0300

    Conectado no banco e pegando dados de la

--- cmd/main.go
+++ cmd/main.go
@@ -4,20 +4,16 @@ import (
 	"github.com/Dankei/conta-click/controller"
 	"github.com/Dankei/conta-click/db"
 	"github.com/Dankei/conta-click/repository"
+	"github.com/Dankei/conta-click/router"
 	"github.com/Dankei/conta-click/usecase"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
-	server.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 
 	dbConnection, err := db.ConnectDB()
-	if(err != nil) {
+	if err != nil {
 		panic(err)
 	}
 	defer dbConnection.Close()
@@ -25,7 +21,6 @@ func main() {
 	ClickRepository := repository.NewClickRepository(dbConnection)
 	ClickUsecase := usecase.NewClickUsecase(*ClickRepository)
 	ClickController := controller.NewClickController(*ClickUsecase)
-	server.GET("/click", ClickController.GetClick)
-
+	router.InitRoutes(&server.RouterGroup, *ClickController)
 	server.Run(":8080")
 }
